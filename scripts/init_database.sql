a2c4cb57df4a99ee5d12b3bcc1bb518cc375baf0
es-ship-it <emmanuel.escobar@uao.edu.co>
2026-02-11 17:56:40 -0500

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,11 @@
+-- =====================================================
+-- Database Initialization Script
+-- Project: COVID-19 Vaccination – SDG 3 Analysis
+-- Description:
+-- This script creates the main Data Warehouse database
+-- used to store and analyze COVID-19 vaccination data
+-- for SDG 3 (Good Health and Well-being) evaluation.
+-- =====================================================
+
+Create database datawarehouse;
+use datawarehouse;
